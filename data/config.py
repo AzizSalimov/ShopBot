06f0dc1e19ee72047961a93c617df6987ee3af5d
salimov <azizsalimov309@gmail.com>
2023-07-06 12:14:41 +0500

    My new bot. Bot name ShopBot

---
+++ data/config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = '6385019069:AAFxRYzQLaJ5kU-dFvFagAIHKkfMndaY7qk'
+
+PROJECT_NAME = 'store-bot-example'
+
+WEBHOOK_HOST = f"https://{PROJECT_NAME}.herokuapp.com"
+WEBHOOK_PATH = '/webhook/' + BOT_TOKEN
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+ADMINS = [5387742201]
